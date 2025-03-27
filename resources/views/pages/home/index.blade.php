e434281acca585b666b7e6c1d8a0921e1b31576f
AN-BlackRiver <anblackriver@gmail.com>
2025-03-27 17:22:57 +0500

    search input make

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -2,18 +2,26 @@
 
 
 @section('content')
-    <div class="container mx-auto px-6 mt-20 bg-white dark:bg-gray-900 pb-4 rounded-md min-h-screen">
+    <div class="container mx-auto px-4 mt-20 bg-white dark:bg-gray-900 pb-4 rounded-md min-h-screen">
         <div class="border-b border-b-gray-200 dark:border-b-gray-600 flex justify-between items-center py-4 mb-4">
-            <div>
-                <h1 class="text-xl font-semibold text-black/80 dark:text-white">Новости</h1>
+            <div class="flex justify-between items-center">
+                <h1 class="text-xl font-semibold text-black/80 dark:text-white mr-4">Новости</h1>
+                <button type="button"
+                        class="text-white bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Создать
+                </button>
             </div>
-            <div>
-                <a href="{{route('login')}}">
-                    <button type="button"
-                            class="text-white bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Добавить новость
-                    </button>
-                </a>
+            <div class="flex justify-between items-center">
+                <input
+                    type="text"
+                    name="q"
+                    class="mr-4 block w-full px-3 py-2 dark:bg-gray-800 border dark:text-white border-gray-300 dark:border-blue-700 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-sm"
+                    placeholder="Поиск"
+                />
+                <button type="button"
+                        class="text-white bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Найти
+                </button>
             </div>
         </div>
         <div class="flex flex-col space-y-4">
