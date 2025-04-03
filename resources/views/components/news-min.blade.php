a69b21a675e76f1d6f9a50e723092d00cbc7da57
AN-BlackRiver <anblackriver@gmail.com>
2025-04-03 11:30:03 +0500

    feat: Доблевно кем опубликованна новость

--- resources/views/components/news-min.blade.php
+++ resources/views/components/news-min.blade.php
@@ -1,20 +1,28 @@
 
 <div class="bg-gray-100/50 dark:bg-gray-800 border-2 border-transparent hover:border-blue-700 transition-colors duration-300 rounded-lg shadow-md overflow-hidden flex">
     <!-- Основной контент -->
-    <div class="p-4 flex-1">
+  {{--  <div class="p-4 flex-1">
         <div class="flex justify-between">
             <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200 mb-2">{{ $news['title'] }}</h2>
             <p class="text-sm text-blue-700 dark:text-blue-100/40">{{ Carbon\Carbon::parse($news['created_at'])->format('d.m.Y H:i')}}</p>
         </div>
 
         <p class="text-gray-600 dark:text-blue-300">{{mb_strlen($news['description']) > 355 ? mb_substr($news['description'], 0, 355) . "..." : $news['description'] }}</p>
+        <p class="text-sm text-gray-600 dark:text-blue-300">опубликовал: <span class="text-blue-500">{{$news['user']['subdivision']['title']}}</span></p>
+    </div>--}}
+    <div class="p-4 flex-1 grid grid-rows-[auto_1fr_auto] min-h-[150px]">
+        <div class="flex justify-between">
+            <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200 mb-2">{{ $news['title'] }}</h2>
+            <p class="text-sm text-blue-700 dark:text-blue-100/40">{{ Carbon\Carbon::parse($news['created_at'])->format('d.m.Y H:i')}}</p>
+        </div>
 
+        <p class="text-gray-600 dark:text-blue-300">{{mb_strlen($news['description']) > 355 ? mb_substr($news['description'], 0, 355) . "..." : $news['description'] }}</p>
+        <p class="mt-3 text-sm text-gray-600 dark:text-gray-400">опубликовал: <span class="text-blue-500">{{$news['user']['subdivision']['title']}}</span></p>
     </div>
-
     <!-- Блок с файлами (только если они есть) -->
     @if(count($news['files']) > 0)
         <div class="w-3/12 border-l border-gray-200 dark:border-gray-700 p-4 bg-gray-50 dark:bg-gray-700/50">
-            <h3 class="font-medium text-gray-700 dark:text-gray-300 mb-2">Прикрепленные файлы:</h3>
+            <h3 class="font-medium text-gray-700 dark:text-gray-300 mb-2">Прикреплённые файлы:</h3>
             <ul class="space-y-2">
                 @foreach($news['files'] as $file)
                     <li class="flex items-center">
