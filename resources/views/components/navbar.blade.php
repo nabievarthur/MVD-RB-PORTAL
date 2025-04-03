2945e4cff7b4059e833e979d6fb6e0630d04b29f
AN-BlackRiver <anblackriver@gmail.com>
2025-04-03 15:57:38 +0500

    fix: навбар ссылки

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -32,7 +32,7 @@
         <!-- Навигационное меню -->
         <div class="absolute left-1/2 transform -translate-x-1/2">
             <div class="flex items-center justify-between" id="navbar-sticky">
-                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 space-x-6 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     @auth
                         <li>
                             <x-nav-link
