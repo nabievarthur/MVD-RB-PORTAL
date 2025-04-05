4c517635578a8b66d6d85b7ca4949053a9e1edef
AN-BlackRiver <anblackriver@gmail.com>
2025-04-05 16:44:06 +0500

    feat: локал скоуп для отображения полених новостей со связями

--- app/Models/News.php
+++ app/Models/News.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,10 +11,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class News extends Model
 {
 
-    /*public function getCreatedAtAttribute($value)
+    public function scopeLatestWithUser(Builder $query): Builder
     {
-        return Carbon::parse($value)->format('d.m.Y H:i');
-    }*/
+        return $query->with('user')
+            ->orderBy('created_at', 'desc');
+    }
     public function files(): HasMany
     {
         return $this->hasMany(File::class);
