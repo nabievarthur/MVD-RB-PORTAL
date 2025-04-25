bc51d83aeeb10f2dc33f16ad222a1a500bf60e55
AN-BlackRiver <anblackriver@gmail.com>
2025-04-25 12:06:38 +0500

    fix: удаление файла из storage при удалении из окна редактирования

--- app/Models/File.php
+++ app/Models/File.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 class File extends Model
 {
     public function news(): BelongsTo
     {
         return $this->belongsTo(News::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::deleting(function (self $model) {
+            Storage::disk('public')->delete($model->path);
+        });
     }
 }
