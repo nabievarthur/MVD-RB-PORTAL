1c3bef403ec4ee3e73320b9fdaaa4722e76ddb59
blackriver <anblackriver@gmail.com>
2025-06-17 16:05:59 +0500

    feat: вывод информации о неуспещном поиске

--- resources/views/pages/admin/user/index.blade.php
+++ resources/views/pages/admin/user/index.blade.php
@@ -247,7 +247,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach($users as $user)
+                    @forelse($users as $user)
                         <tr class="border-b border-gray-600 hover:bg-gray-600 transition-colors">
                             <td class="py-3 px-4">{{$user->id}}</td>
                             <td class="py-3 px-4">{{$user->login}}</td>
@@ -280,7 +280,21 @@
                                 </button>
                             </td>
                         </tr>
-                    @endforeach
+                    @empty
+                        <td colspan="7" class="py-3 px-4 text-2xl text-center">
+                            <div class="flex justify-center items-center space-x-2">
+                                <span>По вашему запросу ничего не найдено</span>
+                                <span>
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor"
+                                         class="bi bi-emoji-frown" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                          <path
+                                         d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
+                                         </svg>
+                                 </span>
+                            </div>
+                        </td>
+                    @endforelse
                     </tbody>
                 </table>
             </div>
